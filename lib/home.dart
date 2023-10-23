d4be6da481e6980bbe25e4b0edc6e053661875da
julie.pouny <pouny.julie@gmail.com>
2023-10-23 21:34:19 +0200

    Login page - modification Modification de la page

--- lib/home.dart
+++ lib/home.dart
@@ -12,4 +12,30 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.grey[200]),
+              onPressed: () {
+                Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
+                  return LoginPage();
+                }));
+              },
+              child: Text("Connexion"),
+            ),
+          ],
+        ),
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
 }
--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:passwordfield/passwordfield.dart';
 
 /*
 Login Page
@@ -20,62 +19,79 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  late String email;
+  late String password;
+  bool _isVisible = false;
+
 
   @override
   Widget build (BuildContext context) {
+    double taille = MediaQuery.of(context).size.width * 0.5;
 
     return GestureDetector(
         onTap: (() => FocusScope.of(context).requestFocus(FocusNode())),
         child: Scaffold(
           body: SingleChildScrollView(
-            padding: EdgeInsets.all(15.0),
+            padding: EdgeInsets.all(40.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                padding(20),
-                //Logo Fur Meet-------------------------------------
-                padding(5),
-                Text(
-                  'Fur Meet',
-                  style: TextStyle(
-                    color: Colors.blueAccent,
-                    fontSize: 25
+                padding(80),
+                Container(
+                  height: taille,
+                  width: taille,
+                  decoration: const BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.pinkAccent,
+                        blurRadius: 10,
+                        offset: Offset(4, 9), // Shadow position
+                      ),
+                    ],
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage('lib/images/furmeet/logo_furmeet.png')
+                    ),
                   ),
                 ),
                 padding(20),
+                const Text(
+                  'Fur Meet',
+                  style: TextStyle(
+                    color: Colors.pinkAccent,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                padding(50),
                 TextField(
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     labelText: 'E-mail'
                   ),
+                  onChanged: (String string){
+
+                  },
                 ),
+                padding(10),
                 //Password => modifier le
-                PasswordField(
-                  color: Colors.blue,
-                  passwordConstraint: r'.*[@$#.*].*',
-                  hintText: 'doit contenir des caractères spéciaux',
-                  border: PasswordBorder(
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.blue.shade100,
-                      ),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.blue.shade100,
-                      ),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    focusedErrorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide:
-                      BorderSide(width: 2, color: Colors.red.shade200),
+                TextField(
+                  obscureText: !_isVisible,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Mot de passe',
+                    suffixIcon: IconButton(
+                      icon: Icon(_isVisible ? Icons.visibility_off : Icons.visibility),
+                      onPressed: () => setState(() {
+                        _isVisible = !_isVisible;
+                      }),
                     ),
                   ),
-                  errorMessage:
-                  'Doit contenir l\'un des caractères suivant: . * @ # \$',
+                  onChanged: (String string){
+
+                    },
                 ),
+                padding(10),
                 padding(15),
                 ElevatedButton(
                   //Définir le onPressed pour la connexion
@@ -84,13 +100,18 @@ class _LoginPageState extends State<LoginPage> {
                       //chemin vers page Hello (voir feuille connexion)
                     });
                   },
-                  child: Text('Connexion'),
+                  child: Text('Connexion',  style: TextStyle(color: Colors.white, fontSize: 25),),
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.pinkAccent,
-                    //-------------------------------------------------------------------- taille fixe boutton
-                    //fixedSize: MaterialStateProperty.all(20),
+                    fixedSize: const Size(300, 50),
+                      side: const BorderSide(
+                          width: 2,
+                          color: Colors.pink
+                      )
                   ),
+
                 ),
+                padding(10),
                 ElevatedButton(
                   //Définir le onPressed pour la connexion
                   onPressed: (){
@@ -98,13 +119,35 @@ class _LoginPageState extends State<LoginPage> {
                       //chemin vers page Hello (voir feuille connexion)
                     });
                   },
-                  child: Text('Pas encore de compte ?'),
+                  child: Text(
+                    'Inscription',
+                    style: TextStyle(color: Colors.pink, fontSize: 23),
+                  ),
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blueAccent,
-                    //-------------------------------------------------------------------- taille fixe boutton
-                    //fixedSize: MaterialStateProperty.all(20),
+                    backgroundColor: Colors.white,
+                    fixedSize: const Size(300, 50),
+                    side: const BorderSide(
+                      width: 2,
+                      color: Colors.pinkAccent
+                    )
                   ),
                 ),
+                padding(30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+
+                        onPressed: (){
+                          //Chemin vers mdp oublié
+                        },
+                        child: Text(
+                          'Mot de passe oublié ?',
+                          style: TextStyle(color: Colors.pinkAccent, fontSize: 20),
+                        )
+                    ),
+                  ],
+                )
               ],
             ),
           ),
@@ -115,4 +158,5 @@ class _LoginPageState extends State<LoginPage> {
   Padding padding(double top){
     return Padding(padding: EdgeInsets.only(top: top));
   }
+
 }
